c66dd66a43169cfad6522b68c6387f65a0d900e0
Prn-Ice <princenna77@gmail.com>
2022-06-24 22:30:13 +0100

    feat(auth): working login and signup with jwt's

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,72 @@
-import { Injectable } from '@nestjs/common';
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import * as argon from 'argon2';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
 
-  signup() {
-    return { message: 'I have signed up' };
+  async signup(dto: AuthDto) {
+    // generate password hash
+    const hash = await argon.hash(dto.password);
+
+    try {
+      // save user in db
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          hash,
+        },
+      });
+
+      // return saved user
+      return this.loginToken(user.id, user.email);
+    } catch (error) {
+      if (error instanceof PrismaClientKnownRequestError) {
+        if (error.code === 'P2002') {
+          throw new ForbiddenException('Credentials taken');
+        }
+      }
+      throw error;
+    }
   }
 
-  login() {
-    return { message: 'I have logged in' };
+  async login(dto: AuthDto) {
+    // find the user by email
+    // if user does not exist throw exception
+    const user = await this.prisma.user.findUnique({
+      where: { email: dto.email },
+    });
+    if (!user) throw new ForbiddenException('Credentials incorrect');
+
+    // compare password
+    const pwMatches = await argon.verify(user.hash, dto.password);
+    // if password incorrect, throw exception
+    if (!pwMatches) throw new ForbiddenException('Credentials incorrect');
+
+    // send back user
+    return this.loginToken(user.id, user.email);
+  }
+
+  async loginToken(
+    userId: number,
+    email: string,
+  ): Promise<{ access_token: string }> {
+    const payload = { sub: userId, email: email };
+    const secret = this.config.get('JWT_SECRET');
+    const token = await this.jwt.signAsync(payload, {
+      expiresIn: '15m',
+      secret: secret,
+    });
+
+    return { access_token: token };
   }
 }
